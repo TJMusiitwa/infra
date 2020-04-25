bdbc3167e08548a9ab1b8cae8ad28e540d7ea63e
Josh Gummersall <josh@standardlabs.dev>
2020-04-25 10:15:03 -0700

    ElevatedNotary cloud function, refactor

--- config_file.tf
+++ config_file.tf
@@ -8,15 +8,16 @@ resource "google_storage_bucket_object" "config_file" {
     "aggS2Levels" = [8, 10, 12]
     "archiveBucket" = google_storage_bucket.archive.name
     "compareS2Level" = 18
+    "elevatedNotaryUrl" = google_cloudfunctions_function.notary.https_trigger_url
     "exposureBucket" = google_storage_bucket.exposures.name
     "exposureS2Level" = 10
-    "timeResolution" = 15
     "holdingBucket" = google_storage_bucket.locations.name
     "notaryUrl" = google_cloudfunctions_function.notary.https_trigger_url
     "operatorUrl" = google_cloudfunctions_function.operator.https_trigger_url
     "publishedBucket" = google_storage_bucket.publish.name
     "reportS2Level" = 22
     "symptomBucket" = google_storage_bucket.symptoms.name
+    "timeResolution" = 15
     "tokenBucket" = google_storage_bucket.bluetooth.name
   })
 }
