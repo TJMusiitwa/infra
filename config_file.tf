d53e854a67fcf7a4a75d63c6ec704768950f1f5f
Josh Gummersall <josh@standardlabs.dev>
2020-04-25 09:32:22 -0700

    Clean up, add test-tokens bucket, elevated notary

--- config_file.tf
+++ config_file.tf
@@ -4,18 +4,19 @@ resource "google_storage_bucket_object" "config_file" {
   cache_control = "private"
 
   content = jsonencode({
-    "notaryUrl" = google_cloudfunctions_function.notary.https_trigger_url
-    "operatorUrl" = google_cloudfunctions_function.operator.https_trigger_url
-    "archiveBucket" = google_storage_bucket.archive.name
-    "exposureBucket" = google_storage_bucket.exposures.name
-    "holdingBucket" = google_storage_bucket.locations.name
-    "symptomBucket" = google_storage_bucket.symptoms.name
-    "publishedBucket" = google_storage_bucket.publish.name
-    "tokenBucket" = google_storage_bucket.bluetooth.name
     "aggS2Index" = 0
     "aggS2Levels" = [8, 10, 12]
+    "archiveBucket" = google_storage_bucket.archive.name
     "compareS2Level" = 18
-    "reportS2Level" = 22
+    "exposureBucket" = google_storage_bucket.exposures.name
     "exposureS2Level" = 10
+    "timeResolution" = 15
+    "holdingBucket" = google_storage_bucket.locations.name
+    "notaryUrl" = google_cloudfunctions_function.notary.https_trigger_url
+    "operatorUrl" = google_cloudfunctions_function.operator.https_trigger_url
+    "publishedBucket" = google_storage_bucket.publish.name
+    "reportS2Level" = 22
+    "symptomBucket" = google_storage_bucket.symptoms.name
+    "tokenBucket" = google_storage_bucket.bluetooth.name
   })
 }
