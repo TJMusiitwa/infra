b0a1b022200a74f4d9aa9c3ec4e37a10c02eae75
Wes Carr <wescarr@gmail.com>
2020-04-27 17:31:28 -0700

    Add new exposure keys buckets to support Google/Apple tracing API

--- storage.tf
+++ storage.tf
@@ -115,6 +115,53 @@ resource "google_storage_bucket_iam_member" "bluetooth_cloudrun" {
   member = "serviceAccount:${google_service_account.cloudrun.email}"
 }
 
+resource "google_storage_bucket" "exposure_keys_holding" {
+  name               = "covidtrace-exposure-keys-holding"
+  location           = "US"
+  force_destroy      = true
+  bucket_policy_only = false
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 1
+    }
+  }
+}
+
+resource "google_storage_bucket_iam_member" "exposure_keys_notary" {
+  bucket = google_storage_bucket.exposure_keys_holding.name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_service_account.notary.email}"
+}
+
+resource "google_storage_bucket_iam_member" "exposure_keys_cloudrun" {
+  bucket = google_storage_bucket.exposure_keys_holding.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.cloudrun.email}"
+}
+
+resource "google_storage_bucket" "exposure_keys_published" {
+  name               = "covidtrace-exposure-keys-published"
+  location           = "US"
+  force_destroy      = true
+  bucket_policy_only = false
+}
+
+resource "google_storage_bucket_iam_member" "exposure_keys_publish_cloudrun" {
+  bucket = google_storage_bucket.exposure_keys_published.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.cloudrun.email}"
+}
+
+resource "google_storage_bucket_iam_member" "exposure_keys_publish_allusers" {
+  bucket = google_storage_bucket.exposure_keys_published.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
 resource "google_storage_bucket" "publish" {
   name               = "covidtrace-published"
   location           = "US"
